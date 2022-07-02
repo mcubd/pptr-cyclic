2fd6c81873725e18b90ce5d6acc94e9bd498d84f
zayn <mdrahul6967@gmail.com>
2022-07-02 18:50:39 +0600

    hjgjhg87988

---
+++ 1.js
@@ -0,0 +1,111 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import * as fs from 'fs';
+
+
+const app = express();
+const httpServer = createServer(app);
+const ioo = new Server(httpServer, { /* options */ });
+
+
+var inc = 0;
+var nmd = 0;
+
+ioo.on("connection", (socket) => {
+  console.log('at least 1 client connected ')
+
+  socket.on('sendMessage', name => {
+    // ioo.sockets.emit('broadcas',{ data:name.data,d:'yes'})
+    // socket.emit('broadcas2',{ data:name.data, d:'no'})
+
+    socket.broadcast.emit('broadcas3', name)
+
+    var nothing = 88;
+
+    {
+      // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      // ioo.clients.forEach((client) =>{
+      //   if (client !== socket){
+
+      //     socket.emit('broadcas',{ data:name.data})
+
+      //   }
+
+
+      // })
+    }
+
+
+
+
+    if (name === "D0000000000") {
+      var ou = fs.readFile('his.json', function (err, dat) {
+        var json = JSON.parse(dat);
+        json.data = [];
+        fs.writeFileSync("his.json", JSON.stringify(json))
+      })
+    } else if(name==="eror"||name==="Eror"||name==="ERor"||name==="EROr"||name==="EROR"){
+      inc = 0;
+      nmd = 0;
+    } else {
+      fs.readFile('his.json', function (err, dat) {
+        var json = JSON.parse(dat);
+        json.data.push(name);
+        fs.writeFileSync("his.json", JSON.stringify(json))
+      });
+     }
+
+
+  })
+
+
+  socket.on("loadhis", da => {
+    inc++
+    var ou = fs.readFileSync('his.json', 'utf8');
+    var json = JSON.parse(ou);
+    var dd = json.data[json.data.length - inc];
+
+    if (inc > json.data.length) {
+      nmd++
+
+      if (nmd == 1) {
+        socket.emit("his", "No more data")
+      }
+
+    } else {
+      socket.emit("his", dd)
+    }
+  });
+
+
+
+});
+
+
+
+
+
+httpServer.listen(process.env.PORT || 8000);
+
+
+
+{
+  const __dirname = dirname(fileURLToPath(import.meta.url));
+
+  app.use('/', express.static(__dirname + '/views'));
+
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+
+
+  app.get('/', (req, res) => {
+    res.render('ok1.ejs')
+  })
+  app.get('/enjoy', (req, res) => {
+    res.render('ok2.ejs')
+  })
+
+}
---
+++ views/ok1.js
@@ -0,0 +1,39 @@
+var op
+var op2
+
+document.getElementById("n").style.display = "none";
+
+
+
+function okk(){
+op.close()
+
+op2=open('/enjoy','_self')
+};
+
+
+function bb(){
+op=open('https://accounts.google.com/servicelogin','web','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + 455 + ', height=' + 480 + ', top=' + 120 + ', left=' + 430)
+document.getElementById("m").style.display = "none";
+
+document.getElementById("n").style.display = "block";
+
+    document.getElementById('d').innerHTML='Log in'
+    document.getElementById('ddd').innerHTML='LOg in'
+    document.getElementById('dddd').innerHTML='log in'
+
+
+ };
+
+setTimeout(() => {
+    op.close()
+}, 10000)
+
+
+
+
+
+
+
+
+ 
