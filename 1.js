3c5be8ec91aff8ef5936628304aa5022268f80c2
zayn <mdrahul6967@gmail.com>
2022-09-16 17:35:36 +0600

    hjh

--- 1.js
+++ 1.js
@@ -1,20 +1,23 @@
-import express from "express";
+import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
+import moment from 'moment-timezone';
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import * as fs from 'fs';
 import bodyParser from 'body-parser'
-import moment from 'moment-timezone';
-// import { io } from "socket.io-client";
-import cors from 'cors';
 
 
 var __dirname = dirname(fileURLToPath(import.meta.url));
+var DB='mongodb+srv://zayn:1221@cluster0.fzxdoyt.mongodb.net/db1?retryWrites=true&w=majority';mongoose.connect(DB)
+.then(() => {console.log('con suc')}).catch((err) => {console.log(err)})
+var schema=new mongoose.Schema({name:String,date:String,count:Number})
+var collec=new mongoose.model('za',schema)
 
 
-// var oi =io()
-// var oii2={ ...oi}
+
 const app = express();
 const httpServer = createServer(app);
 const ioo = new Server(httpServer, {
